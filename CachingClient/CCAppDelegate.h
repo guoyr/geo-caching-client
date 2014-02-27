f6ac7ba22506f46398b48416166b73f64f32ee43
Robert Guo <robertguo@me.com>
2014-02-26 21:58:46 -0500

    Initial Commit

---
+++ CachingClient/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  CachingClient
+//
+//  Created by Robert Guo on 2/26/14.
+//  Copyright (c) 2014 Robert Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CachingClientTests/CachingClientTests.m
@@ -0,0 +1,34 @@
+//
+//  CachingClientTests.m
+//  CachingClientTests
+//
+//  Created by Robert Guo on 2/26/14.
+//  Copyright (c) 2014 Robert Guo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CachingClientTests : XCTestCase
+
+@end
+
+@implementation CachingClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
